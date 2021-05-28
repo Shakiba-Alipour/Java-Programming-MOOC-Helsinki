b1c7a58b5ba87d8e88f8833f4264d3857fe63751
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-05-28 15:43:37 +0430

    Create 09.NumberOfNegativeNumbers.java

---
+++ Part 02/09.NumberOfNegativeNumbers.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+ 
+public class NumberOfNegativeNumbers {
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ct = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == 0) {
+                break;
+            }
+            if (num < 0) {
+                ct = ct + 1;
+            }
+        }
+        System.out.println("Number of negative numbers: " + ct);
+ 
+    }
+}
+ 
