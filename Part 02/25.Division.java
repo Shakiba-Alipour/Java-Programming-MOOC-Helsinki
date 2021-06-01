d4cabe687d0a93e410e0815f41360f4b84c4c5a6
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-01 20:40:51 +0430

    Create 25.Division.java

---
+++ Part 02/25.Division.java
@@ -0,0 +1,18 @@
+ 
+public class Division {
+ 
+    public static void main(String[] args) {
+        
+        // Once you have implemented the division method, you can
+        // try it out here. Fopr example division(3,5);
+        // should print "0.6"
+ 
+        // division(3, 5);
+        division(3,5);
+    }
+ 
+    // implement the method here
+    public static void division(int one,int two){
+        System.out.println(((1.0*one)/two));
+    }
+}
