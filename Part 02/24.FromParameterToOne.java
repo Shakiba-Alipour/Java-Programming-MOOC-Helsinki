c935d16051847d6447fc354e6c26d225d85ed379
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-01 20:40:26 +0430

    Create 24.FromParameterToOne.java

---
+++ Part 02/24.FromParameterToOne.java
@@ -0,0 +1,14 @@
+ 
+public class FromParameterToOne {
+ 
+    public static void main(String[] args) {
+        printFromNumberToOne(5);
+    }
+    
+    public static void printFromNumberToOne(int number){
+        for(int i=number;i>0;i--){
+            System.out.println(i);
+        }
+    }
+ 
+}
