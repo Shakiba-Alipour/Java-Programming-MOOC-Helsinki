d5bc5fb78c79342cdd5a8cef40b1fa49458463f8
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-04 11:18:50 +0430

    Create Room.java

---
+++ Part 04/04.Room/Room.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Room {
+    private String code;
+    private int seats;
+    
+    public Room(String classCode,int numberOfSeats){
+        this.code=classCode;
+        this.seats=numberOfSeats;
+    }
+}
+ 
