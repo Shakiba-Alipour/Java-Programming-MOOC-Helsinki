801d24a56c93420e650750a79e60bdeee5f2bb16
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-04 11:22:18 +0430

    Create Product.java

---
+++ Part 04/07.Product/Product.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    public Product (String initialName, double initialPrice, int initialQuantity){
+        this.name=initialName;
+        this.price=initialPrice;
+        this.quantity=initialQuantity;
+    }
+    public void printProduct(){
+        System.out.println(this.name+", price"+this.price+", "+this.quantity+"pcs");
+    }
+}
+ 
