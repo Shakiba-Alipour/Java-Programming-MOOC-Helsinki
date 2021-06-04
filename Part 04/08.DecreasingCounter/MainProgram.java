1a86bc3c8975fb3acedde47ca6cbcaa6efa79328
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-04 11:23:20 +0430

    Create MainProgram.java

---
+++ Part 04/08.DecreasingCounter/MainProgram.java
@@ -0,0 +1,12 @@
+
+public class MainProgram {
+ 
+    public static void main(String[] args) {
+        DecreasingCounter counter = new DecreasingCounter(10);
+ 
+        counter.printValue();
+        counter.decrement();
+        counter.decrement();
+    }
+}
+ 
