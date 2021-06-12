e02d1690119386fc0bcf037260fd44759a403ac8
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-13 02:23:14 +0430

    Create Film.java

---
+++ Part 04/11.Film/Film.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Film {
+    private String name;
+    private int ageRating;
+    
+    public Film(String filmName,int filmAgeRating){
+        this.name=filmName;
+        this.ageRating=filmAgeRating;
+    }
+    
+    public String name(){
+        return this.name;
+    }
+    
+    public int ageRating(){
+        return this.ageRating;
+    }
+}
+ 
