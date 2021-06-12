0895c40b9ee784bf9c8fd624fc07414092fe1b3c
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-13 02:29:29 +0430

    Create Items.java

---
+++ Part 04/17.Items/Items.java
@@ -0,0 +1,24 @@
+ 
+import java.util.ArrayList;
+import java.util.Scanner;
+ 
+public class Items {
+ 
+    public static void main(String[] args) {
+        // implement here your program that uses the class Item
+ 
+        ArrayList<Item> items = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            String item=scanner.nextLine();
+            if(item.isEmpty()){
+                break;
+            }
+            items.add(new Item(item));
+        }
+        for(Item item: items){
+            System.out.println(item.toString()); 
+        }
+    }
+}
+ 
