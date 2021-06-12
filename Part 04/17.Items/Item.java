8e84515a14730a07c1c4cf9a37b15ac2998cf463
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-06-13 02:29:06 +0430

    Create Item.java

---
+++ Part 04/17.Items/Item.java
@@ -0,0 +1,26 @@
+ 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+ 
+public class Item {
+ 
+    private String name;
+    private LocalDateTime createdAt;
+ 
+    public Item(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+ 
+        return this.name + " (created at: " + formatter.format(this.createdAt) + ")";
+    }
+}
+ 
