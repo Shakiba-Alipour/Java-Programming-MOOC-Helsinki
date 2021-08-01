6cf9e774e41105edb4fbe6d6ee4d9cab9ecffe59
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-01 16:01:28 +0430

    Create Pet.java

---
+++ Part 05/08.BiggestPetShop/Pet.java
@@ -0,0 +1,21 @@
+ 
+public class Pet {
+ 
+    private String name;
+    private String breed;
+ 
+    public Pet(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public String getBreed() {
+        return breed;
+    }
+ 
+}
+ 
