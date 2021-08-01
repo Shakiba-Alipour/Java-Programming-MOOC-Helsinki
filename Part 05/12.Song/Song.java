df7c059188ac9cffdda36c95fd0e367a2e115c3b
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-01 16:10:46 +0430

    Create Song.java

---
+++ Part 05/12.Song/Song.java
@@ -0,0 +1,36 @@
+ 
+public class Song {
+ 
+    private String artist;
+    private String name;
+    private int durationInSeconds;
+ 
+    public Song(String artist, String name, int durationInSeconds) {
+        this.artist = artist;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+    }
+ 
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (!(compared instanceof Song)) {
+            return false;
+        }
+        Song comparedSong = (Song) compared;
+        if (this.name.equals(comparedSong.name)
+                && this.artist.equals(comparedSong.artist)
+                && this.durationInSeconds == comparedSong.durationInSeconds) {
+            return true;
+        }
+        return false;
+    }
+ 
+    @Override
+    public String toString() {
+        return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
+    }
+ 
+}
+ 
