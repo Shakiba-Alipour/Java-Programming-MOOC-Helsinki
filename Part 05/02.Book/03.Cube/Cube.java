74ff8eb272f138c8caad9f63abc03a85a8256318
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-01 15:53:20 +0430

    Create Cube.java

---
+++ Part 05/02.Book/03.Cube/Cube.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Cube {
+ 
+    private int edgeLength;
+ 
+    public Cube(int edgeLength) {
+        this.edgeLength = edgeLength;
+    }
+ 
+    public int volume() {
+        return this.edgeLength * this.edgeLength * this.edgeLength;
+    }
+ 
+    public String toString() {
+        return "The length of the edge is " + this.edgeLength + " and the volume " + volume();
+    }
+}
+ 
