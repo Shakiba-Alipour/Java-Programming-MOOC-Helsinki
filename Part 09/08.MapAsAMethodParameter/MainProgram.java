89e21ece6a0238b522f1fdd46bae744ff174af20
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:50:39 +0430

    Create MainProgram.java

---
+++ Part 09/08.MapAsAMethodParameter/MainProgram.java
@@ -0,0 +1,16 @@
+ 
+import java.util.Map;
+ 
+public class MainProgram {
+ 
+    public static void main(String[] args) {
+        // test your method here
+ 
+    }
+ 
+    // Implement here a method returnSize, which takes a Map-object as a parameter 
+    // and returns the size of the map object
+    public static int returnSize(Map<String,String> map) {
+        return map.size();
+    }
+}
