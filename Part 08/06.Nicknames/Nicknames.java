a3df07948699d1723edc02ee981f33a636ad6387
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:04:13 +0430

    Create Nicknames.java

---
+++ Part 08/06.Nicknames/Nicknames.java
@@ -0,0 +1,17 @@
+ 
+import java.util.HashMap;
+ 
+public class Nicknames {
+    
+    public static void main(String[] args) {
+        // Do the operations required here!
+        
+        HashMap<String, String> nicknames = new HashMap<>();
+        nicknames.put("matthew", "matt");
+        nicknames.put("michael", "mix");
+        nicknames.put("arthur", "artie");
+        System.out.println(nicknames.get("matthew"));
+    }
+    
+}
+ 
