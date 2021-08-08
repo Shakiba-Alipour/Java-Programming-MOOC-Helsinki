dc895fe6e6fc214fb09bf251059e42dad8d11468
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:40:42 +0430

    Create C.java

---
+++ Part 09/01.ABC/C.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class C extends B {
+ 
+    public void c() {
+        System.out.println("C");
+    }
+}
+ 
