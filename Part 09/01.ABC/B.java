4364dc245d661b2b5cf0630f26be377dfa91735a
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:40:20 +0430

    Create B.java

---
+++ Part 09/01.ABC/B.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class B extends A {
+ 
+    public void b() {
+        System.out.println("B");
+    }
+}
+ 
