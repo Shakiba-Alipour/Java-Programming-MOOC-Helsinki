96b3eda515690a4b43952516d8413d793ecfc4ae
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:44:47 +0430

    Create Box.java

---
+++ Part 09/04.DifferentKindsOfBoxes/Box.java
@@ -0,0 +1,16 @@
+ 
+import java.util.ArrayList;
+ 
+public abstract class Box {
+ 
+    public abstract void add(Item item);
+ 
+    public void add(ArrayList<Item> items) {
+        for (Item item : items) {
+            Box.this.add(item);
+        }
+    }
+ 
+    public abstract boolean isInBox(Item item);
+}
+ 
