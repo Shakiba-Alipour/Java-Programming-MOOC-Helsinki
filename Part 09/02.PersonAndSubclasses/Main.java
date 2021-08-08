1c16575e859491ed725bb056187410e01b700d41
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:41:17 +0430

    Create Main.java

---
+++ Part 09/02.PersonAndSubclasses/Main.java
@@ -0,0 +1,16 @@
+ 
+import java.util.ArrayList;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        // write your test code here
+    }
+ 
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person p : persons) {
+            System.out.println(p.toString());
+        }
+    }
+}
+ 
