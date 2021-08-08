8bf1fb9fc726fad8483fb09e125010ad52813e02
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:50:12 +0430

    Create mainProgram.java

---
+++ Part 09/07.ListAsAMethodParameter/mainProgram.java
@@ -0,0 +1,20 @@
+ 
+import java.util.List;
+ 
+ 
+ 
+public class mainProgram {
+ 
+    public static void main(String[] args) {
+        // test your method here
+ 
+    }
+ 
+    // Implement here a method returnSize
+    // which returns the size of the list given to it
+    //as a parameter
+    public static int returnSize(List list){
+        return list.size();
+    }
+}
+ 
