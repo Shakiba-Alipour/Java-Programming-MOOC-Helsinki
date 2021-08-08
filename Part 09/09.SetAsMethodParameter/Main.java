559751bf0672986bb2015f72666d52c54686e364
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-08 22:51:46 +0430

    Create Main.java

---
+++ Part 09/09.SetAsMethodParameter/Main.java
@@ -0,0 +1,17 @@
+ 
+import java.util.Set;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        // You can test your method here
+ 
+    }
+ 
+    // implement the method returnSize here, which returns
+    // the number of elements in the set that it receives as a parameter.
+    public static int returnSize(Set<String> set) {
+        return set.size();
+    }
+ 
+}
