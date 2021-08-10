c44277d61df570dfc316a35d3bb7914d8e85b222
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:18:08 +0430

    Create TextInterface.java

---
+++ Part 11/08.FirstPackages/TextInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mooc.ui;
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class TextInterface implements UserInterface{
+ 
+    @Override
+    public void update() {
+        System.out.println("Updating UI");
+    }
+    
+}
+ 
