96e63db26f7d5a7b2f6cb1b12a23372d659338dd
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:14:09 +0430

    Create Saveable.java

---
+++ Part 11/06.SaveablePerson/Saveable.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public interface Saveable {
+ 
+    default void save() {
+ 
+    }
+ 
+    default void delete() {
+ 
+    }
+ 
+    default void load(String address) {
+ 
+    }
+}
+ 
