ad7b5f6e43029adeec2ea5635dcfab613395f9b3
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:13:14 +0430

    Create Player.java

---
+++ Part 11/05.ThePlayerAndTheBot/Player.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Player {
+ 
+    private String name;
+ 
+    public void play() {
+ 
+    }
+ 
+    public void printName() {
+ 
+    }
+}
+ 
