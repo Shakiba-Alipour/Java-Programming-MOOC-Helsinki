b25d5b0efa7e9bf32af45cd3f568f6caf2f440ba
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:11:34 +0430

    Create Ticket.java

---
+++ Part 11/03.ShowAndTicket/Ticket.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+ 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Ticket {
+ 
+    private int seat;
+    private int code;
+    private Show show;
+}
+ 
