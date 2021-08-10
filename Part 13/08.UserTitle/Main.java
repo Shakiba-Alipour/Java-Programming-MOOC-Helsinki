9236fb8c3124a135d0cda20dccd7d6e9d9d2408a
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 14:40:05 +0430

    Create Main.java

---
+++ Part 13/08.UserTitle/Main.java
@@ -0,0 +1,14 @@
+package title;
+ 
+import java.util.Scanner;
+import javafx.application.Application;
+ 
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Application.launch(UserTitle.class);
+    }
+ 
+}
+ 
