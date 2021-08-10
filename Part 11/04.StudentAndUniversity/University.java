92a893f6bb2460e6459532a929eda3008f9db248
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:12:20 +0430

    Create University.java

---
+++ Part 11/04.StudentAndUniversity/University.java
@@ -0,0 +1,17 @@
+ 
+import java.util.ArrayList;
+ 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author SHAKIBA
+ */
+public class University {
+ 
+    private String name;
+    private ArrayList<Student> students;
+}
