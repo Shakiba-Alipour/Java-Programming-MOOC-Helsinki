47b4a30dbb614b8904493baaffbca77f62356b04
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:15:12 +0430

    Create C.java

---
+++ Part 11/07.BiggerClassDiagram/C.java
@@ -0,0 +1,17 @@
+ 
+import java.util.ArrayList;
+ 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author SHAKIBA
+ */
+public class C extends B implements IC {
+ 
+    private ArrayList<E> es;
+}
+ 
