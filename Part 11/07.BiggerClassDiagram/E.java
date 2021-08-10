7a1a874e93b6cfb2e34f440cd1fd5608e73961b5
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:16:01 +0430

    Create E.java

---
+++ Part 11/07.BiggerClassDiagram/E.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+ 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author SHAKIBA
+ */
+public class E {
+ 
+    private ArrayList<C> cs;
+}
+ 
