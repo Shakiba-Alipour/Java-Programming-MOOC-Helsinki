be446581f97bebf65f96f3299b7ecc3de6564edb
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:08:02 +0430

    Create Plane.java

---
+++ Part 11/02.ABookAndAPlane/Plane.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Plane {
+ 
+    private String ID;
+    private String model;
+    private int yearOfIntroduction;
+}
