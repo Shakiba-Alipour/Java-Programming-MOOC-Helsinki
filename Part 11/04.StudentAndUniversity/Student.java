99494e6ca2bdccc084982631bb692cfc5f6d0d82
Shakiba-Alipour <alipour.sh79@gmail.com>
2021-08-10 13:12:00 +0430

    Create Student.java

---
+++ Part 11/04.StudentAndUniversity/Student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author SHAKIBA
+ */
+public class Student {
+ 
+    private int studentID;
+    private String name;
+    private University uneversity;
+}
